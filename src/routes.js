e20d6e9f6ae5f14b627fa61ec030424dcaaf70ef
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-10-06 18:36:01 -0300

    [Add] Upload de imagem pro perfil do usuario

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,11 @@ import { Router } from 'express';
 import UserController from './app/controllers/userController';
 import SessionController from './app/controllers/sessionController';
 import authMiddleware from './app/middlewares/auth';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', async (req, res) => {
     return res.json({ message: 'server running! :D' });
@@ -15,5 +18,9 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+    return res.json({ ok: true });
+});
 
 export default routes;
