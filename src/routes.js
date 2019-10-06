fbf17056bb7ecf47c4e2f93f4479e89f8ffd8fd8
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-10-06 00:20:47 -0300

    [Add] Controller e rota para criacao de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
+import User from './app/models/user';
+import UserController from './app/controllers/userController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'server running!' });
+routes.get('/', async (req, res) => {
+    return res.json({ message: 'server running! :D' });
 });
+
+routes.post('/users', UserController.store);
 
 export default routes;
