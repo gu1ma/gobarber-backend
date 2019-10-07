ee253dfd67e3746c6c80eb55fbbf21fb64194e0d
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-10-06 21:04:07 -0300

    [Add] Listagem de providers

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/userController';
 import SessionController from './app/controllers/sessionController';
 import FileController from './app/controllers/fileController';
+import ProviderController from './app/controllers/providerController';
 import authMiddleware from './app/middlewares/auth';
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -19,6 +20,7 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
