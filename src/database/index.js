106be6e47e580287299f5556d68b8e092dda45a5
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-10-18 12:57:09 -0300

    [Add] Criando migration e model de agendamento

--- src/database/index.js
+++ src/database/index.js
@@ -4,21 +4,22 @@ import databaseConfig from '../config/database';
 
 import User from '../app/models/user';
 import File from '../app/models/file';
+import Appointment from '../app/models/appointment';
 
-const models = [User, File];
+const models = [User, File, Appointment];
 
-class Database{
-    constructor(){
-        this.init();
-    }
+class Database {
+  constructor() {
+    this.init();
+  }
 
-    init(){
-        this.connection = new Sequelize(databaseConfig);
+  init() {
+    this.connection = new Sequelize(databaseConfig);
 
-        models
-        .map(model => model.init(this.connection))
-        .map(model => model.associate && model.associate(this.connection.models));
-    }
+    models
+      .map((model) => model.init(this.connection))
+      .map((model) => model.associate && model.associate(this.connection.models));
+  }
 }
 
 export default new Database();
