5eb84cbe3f4df54c8aaac6b6d299029f15fc508d
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-10-18 12:10:29 -0300

    [Add] alteracoes efetuadas pelo eslint

--- src/app/controllers/sessionController.js
+++ src/app/controllers/sessionController.js
@@ -1,44 +1,44 @@
-import User from '../models/user';
 import jwt from 'jsonwebtoken';
-import auth from '../../config/auth';
 import * as Yup from 'yup';
+import User from '../models/user';
+import auth from '../../config/auth';
 
 class SessionController {
-    async store(req, res){
-        const { email, password } = req.body;
+  async store(req, res) {
+    const { email, password } = req.body;
 
-        const schema = Yup.object().shape({
-            email: Yup.string().email().required(),
-            password: Yup.string().required(),
-        });
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
 
-        if(!(await schema.isValid(req.body))){
-            return res.status(400).json({ message: 'validation fails' });
-        }
-
-        const user = await User.findOne({ where: { email } });
-
-        if(!user) {
-            return res.status(401).json({ message: 'user not found' });
-        }
-
-        if(!(await user.checkPassword(password))){
-            return res.status(401).json({ message: 'password does not match' });
-        }
-
-        const { id, name } = user;
-
-        return res.json({
-            user: {
-                id,
-                name,
-                email
-            },
-            token: jwt.sign({ id }, auth.secret, {
-                expiresIn: auth.expiresIn,
-            })
-        })
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ message: 'validation fails' });
     }
-} 
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ message: 'user not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ message: 'password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, auth.secret, {
+        expiresIn: auth.expiresIn,
+      }),
+    });
+  }
+}
 
 export default new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,17 @@
 import { Router } from 'express';
 
+import multer from 'multer';
 import UserController from './app/controllers/userController';
 import SessionController from './app/controllers/sessionController';
 import FileController from './app/controllers/fileController';
 import ProviderController from './app/controllers/providerController';
 import authMiddleware from './app/middlewares/auth';
-import multer from 'multer';
 import multerConfig from './config/multer';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.get('/', async (req, res) => {
-    return res.json({ message: 'server running! :D' });
-});
+routes.get('/', async (req, res) => res.json({ message: 'server running! :D' }));
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
