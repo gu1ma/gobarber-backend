eff24c0986ceb4e9e88618b5904c6c1d771e4376
Gabriel Guimarães <gabrielgs1408@gmail.com>
2019-11-03 21:04:12 -0300

    [Add] listando horarios disponiveis

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/userController';
 import SessionController from './app/controllers/sessionController';
 import FileController from './app/controllers/fileController';
 import ProviderController from './app/controllers/providerController';
+import AvailableController from './app/controllers/availableController';
 import AppointmentController from './app/controllers/appointmentController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
@@ -22,7 +23,9 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
